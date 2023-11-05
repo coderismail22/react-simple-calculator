f312a1c4dec643b65538a839563bd3f516dd94c7
ismail <coderismail22@gmail.com>
2023-11-05 22:02:43 +0600

    styles-added

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,41 +1,73 @@
-import React, { useState } from 'react';
-import './Calculator.css';
+import React, { useEffect, useState } from "react";
+import "./Calculator.css";
+const MAX_DISPLAY_LENGTH = 25;
 
 const Calculator = () => {
-  const [display, setDisplay] = useState('');
-  const [result, setResult] = useState('');
+  const [display, setDisplay] = useState("");
+  const [result, setResult] = useState("");
+
+  const buttons = [
+    "7",
+    "8",
+    "9",
+    "/",
+    "4",
+    "5",
+    "6",
+    "*",
+    "1",
+    "2",
+    "3",
+    "-",
+    "0",
+    ".",
+    "=",
+    "+",
+    "C",
+  ];
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      const { key } = event;
+      if (
+        (/[0-9]/.test(key) ||
+          ["+", "-", "*", "/", ".", "=", "Enter"].includes(key)) &&
+        key !== " "
+      ) {
+        handleButtonClick(key);
+      } else if (key === "Escape") {
+        handleButtonClick("C");
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
 
   const handleButtonClick = (value) => {
-    if (value === '=') {
+    if (value === "C") {
+      setDisplay("");
+      setResult("");
+    } else if (value === "=") {
       try {
         setResult(eval(display));
       } catch (error) {
-        setResult('Error');
+        setResult("Error");
       }
-    } else if (value === 'C') {
-      setDisplay('');
-      setResult('');
     } else {
-      setDisplay(display + value);
+      if (display.length < MAX_DISPLAY_LENGTH) {
+        setDisplay((prevDisplay) => prevDisplay + value);
+      }
     }
   };
 
-  const buttons = [
-    '7', '8', '9', '/',
-    '4', '5', '6', '*',
-    '1', '2', '3', '-',
-    '0', '.', '=', '+',
-    'C'
-  ];
-
   return (
     <div className="calculator">
-      <div className="display">
-        {display}
-      </div>
-      <div className="result">
-        {result}
-      </div>
+      <div className="display">{display}</div>
+      <div className="result">{result}</div>
       <div className="buttons">
         {buttons.map((button, index) => (
           <button key={index} onClick={() => handleButtonClick(button)}>
